1525e26ad1c23bfd2588af83d74a356fa8d102e6
kneeshe <cristian_sousa@live.com>
2024-11-02 17:24:33 -0300

    Primeiros arquivos do site

---
+++ pages/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+	<head>
+		<title>O site do Ceis</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia">
+		<link rel="stylesheet" type="text/css" href="../style/style.css">
+		<link rel="icon" type="image/x-icon" href="../imgs/favicon.png">
+	</head>
+	<body>
+		<h1 style="text-align: center"> Bem vindo ao site do Ceis </h1>
+		<h3 style="text-align: center"> --- By Agatha & Cris --- </h3>
+	</body>
+</html>
